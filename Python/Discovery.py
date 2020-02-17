0d3e069b13a35d3c555559d70aa6b83dc8e766b1
Daniel Van Noord <sg1mash98@gmail.com>
2020-02-16 20:41:13 -0500

    Added basic IPv6 support to python examples
    Cleanup of device code
    
    
    Added IPv6 to Python Discovery

--- Python/Discovery.py
+++ Python/Discovery.py
@@ -1,36 +1,84 @@
 import json
 import socket
+import struct
 # requires netifaces, you can install it with pip install netifaces
 # this is used to iterate over all interfaces on the computer and to use the correct broadcast address
 import netifaces
 
-port = 32227 # a temporary port that I choose for testing
+from threading import Lock, Thread
 
-# Create listening port
-# ---------------------
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
-
-try:
-    sock.bind(('0.0.0.0', 0)) #listen to any on a temporary port
-except:
-    print('failure to bind')
-    sock.close()
-    raise
-
+port = 32227  # a temporary port that I choose for testing
 AlpacaDiscovery = "alpacadiscovery1"
 AlpacaResponse = "alpacaport"
 
-for interface in netifaces.interfaces():
-        for interfacedata in netifaces.ifaddresses(interface): 
-                if netifaces.AF_INET == interfacedata:
-                        for ip in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
-                                if('broadcast' in ip):
-                                    sock.sendto(AlpacaDiscovery.encode(), (ip['broadcast'], port))
+lock = Lock()
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    
-    if AlpacaResponse in str(data, "ascii"):  
-        jsondata = json.loads(str(data, "ascii"))  
+
+def search_ipv4():
+    # Create listening port
+    # ---------------------
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    try:
+        sock.bind(('0.0.0.0', 0))  # listen to any on a temporary port
+    except:
+        print('failure to bind')
+        sock.close()
+        raise
+
+    for interface in netifaces.interfaces():
+        for interfacedata in netifaces.ifaddresses(interface):
+            if netifaces.AF_INET == interfacedata:
+                for ip in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
+                    if('broadcast' in ip):
+                        sock.sendto(AlpacaDiscovery.encode(),
+                                    (ip['broadcast'], port))
+
+    while True:
+        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+
+        print_data(str(data, "ascii"), addr)
+
+
+def search_ipv6():
+    # Create listening port
+    # ---------------------
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+
+    try:
+        sock.bind(('', 0))  # listen to any on a temporary port
+    except:
+        print('failure to bind')
+        sock.close()
+        raise
+
+    addrinfo = socket.getaddrinfo("ff02::1", None)[0]
+    group_bin = socket.inet_pton(addrinfo[0], addrinfo[4][0])
+
+    # Join group
+    mreq = group_bin + struct.pack('@I', 0)
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+
+    sock.sendto(AlpacaDiscovery.encode(), ("ff02::1", port))
+
+    while True:
+        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+
+        print_data(str(data, "ascii"), addr)
+
+
+def print_data(data, addr):
+    lock.acquire()
+    if AlpacaResponse in data:
+        jsondata = json.loads(data)
         print(addr[0], ":",  jsondata[AlpacaResponse])
+    lock.release()
+
+
+if __name__ == "__main__":
+    # really basic threading just to get the testing started
+    threadv6 = Thread(target=search_ipv6)
+    threadv6.start()
+    thread = Thread(target=search_ipv4)
+    thread.start()
