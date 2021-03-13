88d068ae5b10ed8eeb27a1c8240351a5cfbe3ccd
Daniel Van Noord <sg1mash98@gmail.com>
2021-03-13 14:23:28 -0500

    Added handling for Windows socket options

--- Dart/alpaca_discovery/lib/alpaca_discovery.dart
+++ Dart/alpaca_discovery/lib/alpaca_discovery.dart
@@ -5,50 +5,91 @@ final String discoveryMessage = 'alpacadiscovery1';
 final String response = '{"alpacaport": 9823}';
 final String multicast_group = 'ff12::00a1:9aca';
 
-bool is_discovery_message(String message){
+bool is_discovery_message(String message) {
   return message.contains(discoveryMessage);
 }
 
 class Responder {
   static Future<RawDatagramSocket> create_ipv4_responder() {
-    final responder = RawDatagramSocket.bind(InternetAddress.anyIPv4, 32227,
-            reuseAddress: true, reusePort: true)
-        .then((RawDatagramSocket udpSocket) {
-      udpSocket.forEach((RawSocketEvent event) {
-        if (event == RawSocketEvent.read) {
-          final dg = udpSocket.receive();
-          print(utf8.decode(dg.data));
-          print(dg.address.toString());
-          print(dg.port.toString());
+    if (Platform.isWindows) {
+      final responder = RawDatagramSocket.bind(InternetAddress.anyIPv4, 32227,
+              reuseAddress: true)
+          .then((RawDatagramSocket udpSocket) {
+        udpSocket.forEach((RawSocketEvent event) {
+          if (event == RawSocketEvent.read) {
+            final dg = udpSocket.receive();
+            print(utf8.decode(dg.data));
+            print(dg.address.toString());
+            print(dg.port.toString());
 
-          if (is_discovery_message(utf8.decode(dg.data))) {
-            udpSocket.send(utf8.encode(response), dg.address, dg.port);
+            if (is_discovery_message(utf8.decode(dg.data))) {
+              udpSocket.send(utf8.encode(response), dg.address, dg.port);
+            }
           }
-        }
+        });
       });
-    });
+      return responder;
+    } else {
+      final responder = RawDatagramSocket.bind(InternetAddress.anyIPv4, 32227,
+              reuseAddress: true, reusePort: true)
+          .then((RawDatagramSocket udpSocket) {
+        udpSocket.forEach((RawSocketEvent event) {
+          if (event == RawSocketEvent.read) {
+            final dg = udpSocket.receive();
+            print(utf8.decode(dg.data));
+            print(dg.address.toString());
+            print(dg.port.toString());
 
-    return responder;
+            if (is_discovery_message(utf8.decode(dg.data))) {
+              udpSocket.send(utf8.encode(response), dg.address, dg.port);
+            }
+          }
+        });
+      });
+      return responder;
+    }
   }
 
   static Future<RawDatagramSocket> create_ipv6_responder() {
-    final responder = RawDatagramSocket.bind(InternetAddress.anyIPv6, 32227,
-            reuseAddress: true, reusePort: true)
-        .then((RawDatagramSocket udpSocket) {
-      udpSocket.joinMulticast(InternetAddress(multicast_group));
-      udpSocket.forEach((RawSocketEvent event) {
-        if (event == RawSocketEvent.read) {
-          final dg = udpSocket.receive();
-          print(utf8.decode(dg.data));
-          print(dg.address.toString());
-          print(dg.port.toString());
+    if (Platform.isWindows) {
+      final responder = RawDatagramSocket.bind(InternetAddress.anyIPv6, 32227,
+              reuseAddress: true)
+          .then((RawDatagramSocket udpSocket) {
+        udpSocket.joinMulticast(InternetAddress(multicast_group));
+        udpSocket.forEach((RawSocketEvent event) {
+          if (event == RawSocketEvent.read) {
+            final dg = udpSocket.receive();
+            print(utf8.decode(dg.data));
+            print(dg.address.toString());
+            print(dg.port.toString());
 
-          if (is_discovery_message(utf8.decode(dg.data))) {
-            udpSocket.send(utf8.encode(response), dg.address, dg.port);
+            if (is_discovery_message(utf8.decode(dg.data))) {
+              udpSocket.send(utf8.encode(response), dg.address, dg.port);
+            }
           }
-        }
+        });
       });
-    });
-    return responder;
+      return responder;
+    } else {
+      final responder = RawDatagramSocket.bind(InternetAddress.anyIPv6, 32227,
+              reuseAddress: true, reusePort: true)
+          .then((RawDatagramSocket udpSocket) {
+        udpSocket.joinMulticast(InternetAddress(multicast_group));
+        udpSocket.forEach((RawSocketEvent event) {
+          if (event == RawSocketEvent.read) {
+            final dg = udpSocket.receive();
+            print(utf8.decode(dg.data));
+            print(dg.address.toString());
+            print(dg.port.toString());
+
+            if (is_discovery_message(utf8.decode(dg.data))) {
+              udpSocket.send(utf8.encode(response), dg.address, dg.port);
+            }
+          }
+        });
+      });
+      return responder;
+    }
   }
 }
+
